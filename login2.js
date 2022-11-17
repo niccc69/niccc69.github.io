a97f6e70733226492effe651c7eb7d7d67e252c6
niccc69 <84114541+niccc69@users.noreply.github.com>
2022-11-17 09:31:26 -0500

    Update login2.js

--- login2.js
+++ login2.js
@@ -3,7 +3,7 @@ function validate()
 var username=document.getElementById("username").value;
 var password=document.getElementById("password").value;
 if(username=="modz"&& password=="cheeks") {
-  window.location='https://www.youtube.com';
+  window.open("https://youtube.com");
 }
 
   
