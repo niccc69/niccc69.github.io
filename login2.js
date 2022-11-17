b036671b11bfa7ac3be3a96c6c22650a33f932ab
niccc69 <84114541+niccc69@users.noreply.github.com>
2022-11-16 21:35:12 -0500

    Update login2.js

--- login2.js
+++ login2.js
@@ -3,6 +3,7 @@ function validate()
 var username=document.getElementById("username").value;
 var password=document.getElementById("password").value;
 if(username=="modz"&& password=="cheeks")
+    window.open("https://niccc69.github.io/ssgjr.html");
 {
     alert("login succesfully");
     return false;
